4217a18228aa0ca680905b16f7dcb67e4e6064e3
Rafal Maziejuk <46377597+rafalmaziejuk@users.noreply.github.com>
2024-10-06 23:05:44 +0200

    add clang-tidy command to CLI tool (#12)
    Signed-off-by: Rafal Maziejuk <rafalmaziejuk@gmail.com>

--- tools/nox_tools/git_utils.py
+++ tools/nox_tools/git_utils.py
@@ -1,12 +1,31 @@
 import git
+import pathlib
 
-def get_repo_root_dir():
+def get_repo_sources():
     """
-    Returns a root directory of this repository.
+    Returns paths to all relevant source files in this repository.
 
     Returns:
         str:
-            repository root directory
+            space-delimited string that contains paths to source files
     """
+    extensions = ('.cpp', 
+                  '.h', 
+                  '.inl')
+    exceptions = ('export.h')
+    dirs = ('include',
+            'sandbox',
+            'src',
+            'tests')
+    
     repo = git.Repo('.', search_parent_directories=True)
-    return repo.working_tree_dir
+    repo_root_dir = repo.working_tree_dir
+
+    sources = []
+    for dir in dirs:
+        dir_path = pathlib.Path(repo_root_dir) / dir
+        sources += [str(path)
+                    for path in dir_path.rglob('*')
+                    if path.suffix in extensions and path.stem not in exceptions]
+
+    return ' '.join(sources)
