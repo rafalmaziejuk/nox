ee1b35ac2f4651d72bad49d79030cce3515b7667
Rafal Maziejuk <rafalmaziejuk@gmail.com>
2024-10-27 20:04:49 +0100

    run formatting check and copyright notice update scripts in pre-commit hook
    Signed-off-by: Rafal Maziejuk <rafalmaziejuk@gmail.com>

--- tools/nox_tools/copyright.py
+++ tools/nox_tools/copyright.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 
-#Copyright 2024 Rafal Maziejuk
-#
-#Licensed under the Apache License, Version 2.0 (the "License");
-#you may not use this file except in compliance with the License.
-#You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-#Unless required by applicable law or agreed to in writing, software
-#distributed under the License is distributed on an "AS IS" BASIS,
-#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#See the License for the specific language governing permissions and
-#limitations under the License.
+# Copyright 2024 Rafal Maziejuk
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import re
 
@@ -90,7 +90,7 @@ def _update_copyright_notice(path):
             index = index + 1
 
         copyright_notice = COPYRIGHT_NOTICE_TEMPLATE.format(year_range=current_year)[1:]
-        copyright_notice_lines = [f"{comment_style}{line}" for line in copyright_notice.splitlines(True)]
+        copyright_notice_lines = [f"{comment_style} {line}" for line in copyright_notice.splitlines(True)]
         lines.insert(index, ''.join(copyright_notice_lines))
 
     with open(path, 'w') as file:
